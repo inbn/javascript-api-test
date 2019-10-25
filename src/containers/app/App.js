adfa794403c01b8682e349b1057310ae115d44df
Carlene Cannon-Conner <carlenecannon-conner@carlenes-mbp.home>
2019-10-25 09:20:01 +0100

    Test API by getting and displaying random brew name on button click

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,19 +1,36 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      randomBrew: ""
     };
   }
 
-  componentDidMount = () => {
+  componentDidMount = () => {};
 
+  onGetRandomBrew = e => {
+    e.preventDefault();
+    axios.get(`https://api.punkapi.com/v2/beers/random`).then(response => {
+      console.log("get random brew response:", response);
+      if (response && response.data && response.data[0]) {
+        this.setState({ randomBrew: response.data[0].name });
+      }
+    });
   };
 
   render = () => {
-
-    return <div>Brewing ...</div>;
+    const { randomBrew } = this.state;
+    return (
+      <section>
+        <form onSubmit={this.onGetRandomBrew}>
+          <input type="submit" value="Get Random Brew" />
+        </form>
+        {randomBrew !== "" && <p>Random Brew: {randomBrew}</p>}
+      </section>
+    );
   };
 }
 
